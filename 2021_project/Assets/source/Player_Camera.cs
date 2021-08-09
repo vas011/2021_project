0aeefc879b926786d85e3b7357817e6bf3d7540a
vas011 <hisg03415@gmail.com>
2021-08-09 15:13:51 +0900

    21.08.09_1
    카메라 좌우 이동 움직임 추가

--- 2021_project/Assets/source/Player_Camera.cs
+++ 2021_project/Assets/source/Player_Camera.cs
@@ -7,6 +7,18 @@ public class Player_Camera : MonoBehaviour
     [SerializeField]
     GameObject player;
 
+    public float speed;
+
+    float datamouseX = 0;
+
+    void camera_move()
+    {
+        float save_mouseX = Input.GetAxis("Mouse X") * speed;
+        datamouseX += save_mouseX;
+
+        transform.eulerAngles = new Vector3(30 , datamouseX, 0);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +28,7 @@ public class Player_Camera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        camera_move();
         transform.position = player.transform.position + new Vector3(0, 5, -5);
     }
 }
