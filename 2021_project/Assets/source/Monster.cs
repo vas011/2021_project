acc3215fb4aa1233bd31cf4f91953be09e46406c
vas011 <hisg03415@gmail.com>
2021-08-18 19:41:26 +0900

    21.08.18 몬스터 시야 설정
    몬스터 시야 설정 코드 수정 필요
    
    정확하게 플레이어를 찾을수 있게 코드 수정이 필요하며 플레이어를 찾을시 플레이어를 계속 따라게 할것으로 생각중

--- 2021_project/Assets/source/Monster.cs
+++ 2021_project/Assets/source/Monster.cs
@@ -8,10 +8,26 @@ public class Monster : MonoBehaviour
     NavMeshAgent navi_Agent;
     Animator monster_Ani;
 
+    //레이마스크 설정 변수
+    [SerializeField]
+    LayerMask player_Layermak = 0;
+    //몬스터 시야 각도
+    [SerializeField]
+    float angle;
+    //몬스터 시야 거리
+    [SerializeField]
+    float distancec;
+
+
+    //웨이포인트 배열 변수
     [SerializeField]
     Transform[] move_point;
 
+    //웨이포인트 카운트
     int count = 0;
+    //플레이어 찾았음을 저장하는 변수
+    bool fiend_player;
+
 
     void monster_move()
     {
@@ -32,6 +48,20 @@ public class Monster : MonoBehaviour
 
     }
 
+    void field_player()
+    {
+        Collider[] colls = Physics.OverlapSphere(transform.position, distancec, player_Layermak);
+
+        if(colls.Length > 0)
+        {
+            fiend_player = true;
+            Debug.Log("플레이어 찾음!");
+        }
+
+        //눈에 보이게 광선의 방향 및 길이 그리기
+        Debug.DrawRay(transform.position , transform.TransformDirection(Vector3.forward)* distancec,Color.red);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -43,6 +73,6 @@ public class Monster : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        field_player();
     }
 }
