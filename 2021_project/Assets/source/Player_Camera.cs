9f0b517aea3914250343e343da50243e21a20b02
vas011 <hisg03415@gmail.com>
2021-08-18 18:11:23 +0900

    21.08.16 게임 카메라 수정
    플레이어 카메라 수정 중
    
    구체적으로 어떻게 진행 할것인지 기획 해야함.

--- 2021_project/Assets/source/Player_Camera.cs
+++ 2021_project/Assets/source/Player_Camera.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class Player_Camera : MonoBehaviour
 {
     [SerializeField]
-    GameObject player;
+    Transform player;
+    [SerializeField]
+    Transform camera_Arm;
 
     public float speed;
-
-    float datamouseX = 0;
-
 
     /* 기능 중지
      * 사용할지 안할지 추후 설정*/
@@ -19,14 +18,23 @@ public class Player_Camera : MonoBehaviour
     void camera_move()
     {
         //마우스의 좌우 움직임 입력값 받아오기
-        float save_mouseX = Input.GetAxis("Mouse X") * speed;
-        //마우스 이동값 저장;;
-        datamouseX += save_mouseX;
-        //카메라 x축 이동 제한 코드 적용
-        datamouseX = Mathf.Clamp(datamouseX, -90, 90);
-        //마우스 이동으로 인한 카메라 시점 이동 코드 적용
-        transform.eulerAngles = new Vector3(30, datamouseX, 0);
+        Vector2 input_mouse = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        //카메라 앵글 값 저장
+        Vector3 camera_Angle = camera_Arm.rotation.eulerAngles;
+        float x = camera_Angle.y + input_mouse.x;
 
+        //카메라 좌우 이동 제한 코드
+        if (x < 180f) // 오른쪽 제한
+        {
+            x = Mathf.Clamp(x , -1 , 90);
+        }
+        else // 왼쪽 제한
+        {
+            x = Mathf.Clamp(x, 280, 365);
+        }
+
+        //마우스 이동으로 인한 카메라 시점 이동 코드 적용
+        camera_Arm.rotation = Quaternion.Euler(camera_Angle.x, x, camera_Angle.z);
     }
     /**/
 
@@ -34,17 +42,14 @@ public class Player_Camera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //기본 카메라 축 초기화
-        datamouseX = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        //카메라 위치 설정 (위치 고정형)
-        transform.position = player.transform.localPosition + new Vector3(0, 5, -5);
         camera_move();
+
+
         //rotation 설정을 Quaternion.Euler() 함수를 통하여 Vector3 x,y,z 축 값을 사용
         //transform.rotation = Quaternion.Euler(30,player.transform.rotation.eulerAngles.y,0);
 
