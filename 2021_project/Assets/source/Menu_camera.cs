3a87c809915cf91ea50ca9c0677587f6f95ea85d
vas011 <hisg03415@gmail.com>
2021-11-01 19:43:41 +0900

    21.11.01 메뉴 UI 작업 완료
    카메라 애니메이션 조정 과 연출 작업 완료 하였으며 버튼 기능 확인 완료
    
    게임 UI 작업에 들어갈려고 아이콘 이미지 와 버튼 UI 추가

--- 2021_project/Assets/source/Menu_camera.cs
+++ 2021_project/Assets/source/Menu_camera.cs
@@ -6,21 +6,56 @@ using UnityEngine.UI;
 public class Menu_camera : MonoBehaviour
 {
     [SerializeField]
-    GameObject panel;
+    Image panel;
+
+    public float fade_speed = 10;
 
     // Start is called before the first frame update
     void Start()
     {
-        panel = GetComponent<GameObject>();
+//        StartCoroutine("fade_onoff");
     }
-    public void panel_hide()
+   
+    public void panel_fade()
     {
-        Debug.Log("하이드 이벤트!");
+        StartCoroutine("fade_onoff");
+    }
+
+    IEnumerator fade_onoff()
+    {
+        Color fade_color = panel.color;
+
+        while (fade_color.a < 1)
+        {
+            Debug.Log("페이드 온 이벤트!");
+            fade_color.a += Time.deltaTime;  // fade_speed;
+            if(fade_color.a >= 1)
+            {
+                fade_color.a = 1;
+            }
+            panel.color = fade_color;
+
+            yield return null;
+        }
+        Debug.Log("페이드 온 이벤트 완료");
+        yield return new WaitForSeconds(2f);
+
+        while (fade_color.a > 0)
+        {
+            Debug.Log("페이드 아웃 이벤트!");
+            fade_color.a -= Time.deltaTime; // fade_speed;
+            if (fade_color.a <= 0)
+            {
+                fade_color.a = 0;
+            }
+            panel.color = fade_color;
+            yield return null;
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
