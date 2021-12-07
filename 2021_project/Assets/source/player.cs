378ac3a43d3c47c162b93be1278b794e9f8da037
vas011 <hisg03415@gmail.com>
2021-12-07 23:39:34 +0900

    21.12.07
    피드백 관련하여 UI 추가할 자료 Improt 하였고
    플레이어 이동은 부드럽게 수정 완료 하였음
    
    넣을 플레이어 UI
    체력 및 스테미너
    공격 이벤트
    나침반(생각중)

--- 2021_project/Assets/source/player.cs
+++ 2021_project/Assets/source/player.cs
@@ -6,6 +6,7 @@ public class player : MonoBehaviour
 {
 
     Animator player_Animator;
+    Rigidbody player_rigidbody;
     [SerializeField]
     Transform player_body;
     [SerializeField]
@@ -31,19 +32,24 @@ public class player : MonoBehaviour
         player_Attack = false;
         player_Animator = GetComponent<Animator>();
         Player_sound = GetComponent<AudioSource>();
+        player_rigidbody = GetComponent<Rigidbody>();
         InvokeRepeating("Attack", 10f, 2f);
     }
 
     void move()
     {
-        Vector3 move_dir = Vector3.zero;
-        move_dir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        Vector3 P_move = transform.forward = camer_arm.forward;
-        if (move_dir.x == 0 && move_dir.z == 0)
+        //Vector3 move_dir = Vector3.zero;        
+        //move_dir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        //Vector3 P_move = transform.forward = camer_arm.forward;
+
+        /* 딱딱한 움직임 
+         * 
+        if (move_dir.magnitude == 0)
         {
             player_move = false;
             player_Animator.SetFloat("speed", 0f);
         }
+
         //상하
         else if (move_dir.x == 0f && move_dir.z != 0)
         {
@@ -79,8 +85,38 @@ public class player : MonoBehaviour
                 transform.position += transform.forward * player_speed * Time.deltaTime;
             }
         }
+
+        /**/
+
+        /* 부드러운 플레이어 움직임 코드 블록 */
+
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        if (moveInput.magnitude == 0)
+        {
+            player_move = false;
+            player_Animator.SetFloat("speed", 0f);
+            moveInput = Vector2.zero;
+        }
+
+        //키입력이 들어올떄
+        if (moveInput.magnitude != 0)
+        {
+            //캐릭터 움직임 인식하게 변수 변경
+            player_move = true;
+            Vector3 lookForward = new Vector3(camer_arm.forward.x, 0f, camer_arm.forward.z).normalized;
+            Vector3 lookRight = new Vector3(camer_arm.right.x, 0f, camer_arm.right.z).normalized;
+            Vector3 smoos_move = lookForward * moveInput.y + lookRight * moveInput.x;
+
+            player_body.forward = smoos_move;
+            transform.position += smoos_move * player_speed * Time.deltaTime;
+            player_Animator.SetFloat("speed", 2f);
+        }
+
+        /**/
+
         //esc 키로 일시중지 키 코드 적용
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             //오브젝트가 가지고 있는 함수를 사용하기 위해 아래와 같이 코드 적용
 
@@ -130,12 +166,12 @@ public class player : MonoBehaviour
             Debug.Log(count.ToString());
         }
     }
-    
+
     void Player_footstep_sound()
     {
         Player_sfx.Player_SFX_Play();
     }
-   
+
     void Update()
     {
         if (player_Attack != false)
