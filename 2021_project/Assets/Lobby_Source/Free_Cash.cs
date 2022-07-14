e62026cd2e68d9a4d7f6e970d287c1746bccc33a
vas011 <hisg03415@gmail.com>
2022-07-14 16:38:08 +0900

    22.07.14
    플레이어 저장데이터를 위해 새로운 스크립트 생성
    
    무기 강화 및 선택을 위해 UI 작업 추가

--- 2021_project/Assets/Lobby_Source/Free_Cash.cs
+++ 2021_project/Assets/Lobby_Source/Free_Cash.cs
@@ -13,8 +13,13 @@ public class Free_Cash : MonoBehaviour
     [SerializeField]
     Text Free_cash_Text;
 
+    [SerializeField]
+    Text Coin_Text;
+    [SerializeField]
+    Text Cash_Text;
+
     GameObject google_ads;
-    
+    GameObject Player_Manager;
 
     int Random_free_cash = 0;
 
@@ -22,6 +27,9 @@ public class Free_Cash : MonoBehaviour
     private void Start()
     {
         google_ads = GameObject.Find("Google_Ads");
+        Player_Manager = GameObject.Find("Player_Manager");
+        Coin_Text.text = ": " + Player_Manager.GetComponent<PlayerManager>().Coin.ToString();
+        Cash_Text.text = ": " + Player_Manager.GetComponent<PlayerManager>().Cash.ToString();
     }
 
     public void Ads_onoff()
@@ -47,7 +55,9 @@ public class Free_Cash : MonoBehaviour
         {
             google_ads.GetComponent<Goolge_Ads>().AdStart();
             Random_free_cash = Random.Range(10,50);
-            Free_cash_Text.text = Random_free_cash.ToString();
+            Free_cash_Text.text = Random_free_cash.ToString(); // 랜덤 다이아 얼마 받았는지 표기
+            Player_Manager.GetComponent<PlayerManager>().Cash += Random_free_cash; // 랜덤값을 플레이어 매니저에 저장
+            Cash_Text.text = ": " + Player_Manager.GetComponent<PlayerManager>().Cash.ToString(); // 랜덤값 메인화면 UI 표기
             Free_plus_Panel.SetActive(true);
         }
     }
