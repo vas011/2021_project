5d3899c6fd3bff314ff141622c726e4a8f9acb82
vas011 <hisg03415@gmail.com>
2022-08-02 18:22:04 +0900

    22.08.02
    UI 관련해서 화면 크기 조정 완료
    
    미니맵 플레이어 마크 새로 만들고 추가
    플레이어 마크 회전 관련해서 스크립트 수정중

--- 2021_project/Assets/source/Minimap_scrips.cs
+++ 2021_project/Assets/source/Minimap_scrips.cs
@@ -7,18 +7,24 @@ public class Minimap_scrips : MonoBehaviour
 {
     [SerializeField]
     GameObject Player_Mark;
+    [SerializeField]
+    GameObject Player_camera_arm;
 
     Transform Player_position;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         Player_position = GameObject.FindGameObjectWithTag("Player").transform;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Player_Mark.transform.position = new Vector3(Player_position.position.x, 55, Player_position.position.z);
+        
+        Player_Mark.transform.position = new Vector3(Player_position.position.x, 65, Player_position.position.z);
+        Player_Mark.transform.rotation = new Quaternion(0, Player_camera_arm.transform.rotation.y, 0,0);
     }
 }
